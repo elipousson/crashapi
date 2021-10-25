9d5d82db9c9269d122462970cf9920a9d00f50d0
Eli Pousson <eli.pousson@gmail.com>
2021-10-25 14:00:21 -0400

    feat: Add table w/ terms and definitions from NHTSA website

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' @title NHSTA Terms and Defintions
+#' @description FARS-related terms defined by the National Highway Traffic
+#'   Safety Administration based on ANSI D16.1-1996: Manual on Classification of
+#'   Motor Vehicle Traffic Accidents.
+#' @format A data frame with 66 rows and 2 variables:
+#' \describe{
+#'   \item{\code{term}}{character Term}
+#'   \item{\code{definition}}{character Term definition}
+#'}
+#' @source \href{https://www-fars.nhtsa.dot.gov/Help/Terms.aspx}{NHTSA FARS Terms}
+"fars_terms"
