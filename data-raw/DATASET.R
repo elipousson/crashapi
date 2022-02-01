91e18776047a3a207cac042b50dcb842b3d157fd
Eli Pousson <eli.pousson@gmail.com>
2022-01-31 23:38:03 -0500

    feat: Add `fars_vars_labels`
    - feat: Add `fars_vars_labels` with names and labels derived from current analytical manual

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,10 @@
 ## code to prepare `DATASET` dataset goes here
-
 fars_terms <-
-  readr::read_csv("inst/extdata/fars_terms.csv")
+ readr::read_csv("inst/extdata/fars_terms.csv")
 
 usethis::use_data(fars_terms, overwrite = TRUE)
+
+fars_vars_labels <-
+  googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1aqRsXKtumgt5umiVsDTC0ETQiNVFZdWaal9ttQZmSNw/edit?usp=sharing")
+
+usethis::use_data(fars_vars_labels, overwrite = TRUE)
