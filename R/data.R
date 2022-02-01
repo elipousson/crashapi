9ebc4b5bbb8cae4d79e1b1096e5a79aea55fe276
Eli Pousson <eli.pousson@gmail.com>
2022-01-31 23:46:12 -0500

    docs: Document `fars_vars_labels` in data.R

--- R/data.R
+++ R/data.R
@@ -9,3 +9,27 @@
 #'}
 #' @source \href{https://www-fars.nhtsa.dot.gov/Help/Terms.aspx}{NHTSA FARS Terms}
 "fars_terms"
+
+
+#' @title FARS variable names and labels
+#' @description A table of FARS table variable names extracted from the Fatality
+#'   Analysis Reporting System (FARS) Analytical User's Manual, 1975-2019,
+#'   documentation of the SAS format data files.
+#' @format A data frame with 498 rows and 14 variables:
+#' \describe{
+#'   \item{\code{data_file}}{character SAS data file name}
+#'   \item{\code{data_file_id}}{double SAS data file ID}
+#'   \item{\code{file_id}}{character File ID}
+#'   \item{\code{label}}{character Variable label}
+#'   \item{\code{name}}{character Variable name}
+#'   \item{\code{location}}{double Location in SAS data file}
+#'   \item{\code{order}}{double Sort order}
+#'   \item{\code{mmuc_equivalent}}{logical Equivalent term in MMUC (placeholder)}
+#'   \item{\code{discontinued}}{logical Indicator for discontinued variables}
+#'   \item{\code{key}}{logical Indicator for key variables}
+#'   \item{\code{api_only}}{logical Indicator for variables only used by API}
+#'   \item{\code{api}}{character Name(s) of corresponding CrashAPI service}
+#'   \item{\code{name_var}}{logical Indicator for "NAME" variable returned by API}
+#'   \item{\code{api_list_col}}{logical Indicator for list columns returned by API}
+#'}
+"fars_vars_labels"
