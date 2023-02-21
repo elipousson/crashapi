17bb5e8fdfd644790a07769ab4e1442a3f511a64
Eli Pousson <eli.pousson@gmail.com>
2023-02-21 10:27:56 -0500

    feat: add nm column to fars_vars_labels

--- data-raw/fars.R
+++ data-raw/fars.R
@@ -1,12 +1,17 @@
-## code to prepare `DATASET` dataset goes here
-fars_terms <-
-  readr::read_csv("inst/extdata/fars_terms.csv")
+fars_terms <- readr::read_csv("inst/extdata/fars_terms.csv")
 
 usethis::use_data(fars_terms, overwrite = TRUE)
 
 fars_vars_labels <-
   googlesheets4::read_sheet(
     "https://docs.google.com/spreadsheets/d/1aqRsXKtumgt5umiVsDTC0ETQiNVFZdWaal9ttQZmSNw/edit?usp=sharing"
+  )
+
+fars_vars_labels <-
+  fars_vars_labels |>
+  dplyr::mutate(
+    nm = janitor::make_clean_names(name),
+    .after = dplyr::all_of("name")
   )
 
 usethis::use_data(fars_vars_labels, overwrite = TRUE)
